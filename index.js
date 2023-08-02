bd6efa6dd18414800a5c2e36bedf76563114a53b
Jakob Unnebäck <jakob@folkdatorn.se>
2023-08-02 16:04:52 +0200

    Fix visual bug that sometimes occured

--- index.js
+++ index.js
@@ -115,8 +115,8 @@ const Flippable = React.forwardRef(
           position: 'absolute',
           top: 0,
           left: 0,
-          width: '100%',
-          height: '100%',
+          right: 0,
+          bottom: 0,
           transform: [
             {
               perspective: 2000
